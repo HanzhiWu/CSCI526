58f9783698552004f5b543a1a40badc6e94fb534
steven.wu <hanzhiwu@usc.edu>
2023-09-16 22:51:04 -0700

    Add CheckPoint Function

---
+++ Assets/__Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace __Scripts
+{
+    public class Bullet : MonoBehaviour
+    {
+        public float life = 1;
+        public GameObject targetToDestroy;
+        public string playerObjectName = "player";
+
+        void Awake()
+        {
+            Destroy(gameObject, life);
+        }
+
+        void Start()
+        {
+            targetToDestroy = GameObject.Find(playerObjectName);
+        }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            //if (collision.gameObject == targetToDestroy)
+            //{
+            //    Destroy(targetToDestroy);
+            //}
+            Destroy(gameObject);
+        }
+    }
+}
